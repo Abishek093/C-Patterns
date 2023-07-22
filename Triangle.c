3090285181ff17a1b1fd02fe7173a754ec6980f6
Abishek K S <99885858+Abishek093@users.noreply.github.com>
2023-07-22 10:54:25 +0530

    Create Triangle.c

---
+++ Triangle.c
@@ -0,0 +1,25 @@
+/*
+
+*
+**
+***
+****
+*****
+
+*/
+
+#include<stdio.h>
+void main()
+{
+    int i, j, row;
+    printf("Enter number of rows: ");
+    scanf("%d", &row);
+    for(i=1; i<=row; i++)
+    {
+        for(j=0; j<i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
