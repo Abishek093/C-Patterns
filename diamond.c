6376b17731d4e636bfbab5aaf6f9de1df076b4f9
Abishek K S <99885858+Abishek093@users.noreply.github.com>
2023-07-22 12:21:07 +0530

    Create diamond.c

---
+++ diamond.c
@@ -0,0 +1,37 @@
+/*
+
+   *
+  * *
+ * * *
+* * * *
+ * * *
+  * *
+   *
+
+*/
+
+
+#include<stdio.h>
+void main()
+{
+    int i, j, row, sp, l;
+    printf("Enter number of rows: ");
+    scanf("%d", &row);
+    for(i=-row; i<=row; i++)
+    {
+        if(i<0)
+            l= -i;
+        else
+            l= i;
+        
+        for(sp=1; sp<=l; sp++)
+        {
+            printf(" ");
+        }
+        for(j=1; j<=row-l+1; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
