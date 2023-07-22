ae03d81da29b0746962eaf193de0aeab8b349576
Abishek K S <99885858+Abishek093@users.noreply.github.com>
2023-07-22 11:10:36 +0530

    Create fullPyramid.c

---
+++ fullPyramid.c
@@ -0,0 +1,30 @@
+/* 
+
+    *
+   * *
+  * * *
+ * * * *
+* * * * *
+
+*/
+
+#include<stdio.h>
+void main()
+{
+    int i, j, row, sp;
+    printf("Enter number of rows: ");
+    scanf("%d", &row);
+    for(i=1; i<=row; i++)
+    {
+        for (sp=1; sp<=row-i; sp++)
+        {
+            printf(" ");
+        }
+        for(j=1;j<=i; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+} 
+
