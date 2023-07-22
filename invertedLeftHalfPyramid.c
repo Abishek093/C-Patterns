932575083837c530b6a982e19cf45702ddc17d5e
Abishek K S <99885858+Abishek093@users.noreply.github.com>
2023-07-22 11:49:38 +0530

    Create invertedLeftHalfPyramid.c

---
+++ invertedLeftHalfPyramid.c
@@ -0,0 +1,33 @@
+
+/*
+
+
+*****
+ ****
+  ***
+   **
+    *
+
+
+*/
+
+
+#include<stdio.h>
+void main()
+{
+    int i, j, row, sp;
+    printf("Enter number of rows: ");
+    scanf("%d", &row);
+    for(i=0; i<row; i++)
+    {
+        for(sp=0; sp<i; sp++)
+        {
+            printf(" ");
+        }
+        for(j=0; j<row-i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
